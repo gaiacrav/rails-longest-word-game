3abd68995c6ef93c7c7bea16e402656fbf09514c
Gaia Cravesana <gcravesana@gmail.com>
2021-02-15 16:47:18 +0000

    implemented the first 3 parts

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
   # The new action will be used to display a new random grid and a form. The form will be submitted (with POST) to the score action.
 
   def new
+    @letters = ('a'..'z').to_a.sample(10)
   end
 
   def score
