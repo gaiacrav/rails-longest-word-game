0ad4cc308f923596c192e0fad88cc4cfeae4850e
Gaia Cravesana <gcravesana@gmail.com>
2021-02-15 16:11:41 +0000

    setup done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+  # The new action will be used to display a new random grid and a form. The form will be submitted (with POST) to the score action.
+
+  def new
+  end
+
+  def score
+  end
+
+end
